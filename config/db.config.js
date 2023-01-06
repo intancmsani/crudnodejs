7821bf85508de4e560f5865cdc79ccd5e62b3391
intancmsani <intancmsaniceria@gmail.com>
2023-01-07 02:22:39 +0700

    versi crud backend v.0.1

---
+++ config/db.config.js
@@ -0,0 +1,14 @@
+const mysql = require("mysql");
+// create here mysql connection
+const dbConn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "node_react",
+  port: 8889,
+});
+dbConn.connect(function (error) {
+  if (error) throw error;
+  console.log("Database Connected Successfully!!!");
+});
+module.exports = dbConn;
